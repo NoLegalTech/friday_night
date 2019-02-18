f925b64c0af897309730d77b433d44e1261f9a6f
Pepe Doval <pepellou@gmail.com>
2019-02-18 13:00:14 -0600

    Cleanup wrong copyright note

--- www/assets/js/crypto-js.js
+++ www/assets/js/crypto-js.js
@@ -1,10 +1,3 @@
-/*!
- * ammana.es - job protocols generator
- * https://github.com/NoLegalTech/ammana
- * Copyright (C) 2018 Zeres Abogados y Consultores Laborales SLP <zeres@zeres.es>
- * https://github.com/NoLegalTech/ammana/blob/master/LICENSE
- */
-
 ;(function (root, factory) {
 	if (typeof exports === "object") {
 		// CommonJS
